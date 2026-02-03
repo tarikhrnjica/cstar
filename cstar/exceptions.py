e60bd92876a33158f39378f5396c41a6994d929e
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-02-03 17:35:24 +0100

    feat: define base error class

--- cstar/exceptions.py
+++ cstar/exceptions.py
@@ -1,4 +1,16 @@
-class ObstructionError(SyntaxError):
-    """Raised when physical constraints (commutativity, cohomology) are violated."""
+class CStarError(Exception):
+    """Base class for all C* language errors."""
+
+    pass
+
+
+class ObstructionError(CStarError):
+    """
+    Raised when a specific definition violates physical laws.
+    Analogue: TypeError, ValueError
+
+    Example:
+        Context('Bad', [X, Z])  # Doesn't commute
+    """
 
     pass
